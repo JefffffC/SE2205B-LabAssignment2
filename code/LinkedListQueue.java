cf12a097c59ba8f76dd5a00676b97953d9e90325
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2019-03-07 18:34:10 -0500

    completed part 1

--- code/LinkedListQueue.java
+++ code/LinkedListQueue.java
@@ -1,14 +1,21 @@
-public class LinkedListQueue<E> implements Queue<E>{
+public class LinkedListQueue<E> implements Queue<E> {
+  private SinglyLinkedList<E> storageList;
   public LinkedListQueue(){
+	storageList = new SinglyLinkedList<E>();
   }
   public int size(){
+	return storageList.size();
   }
   public boolean isEmpty(){
+	return storageList.isEmpty();
   }
   public E first(){
+	return storageList.first();
   }
   public void enqueue(E node){
+	storageList.addLast(node);
   }
   public E dequeue(){
+	return storageList.removeFirst();
   }
 }
--- code/SinglyLinkedList.java
+++ code/SinglyLinkedList.java
@@ -1,38 +1,86 @@
 public class SinglyLinkedList<E>{
 
-  private static class Node<E>{
-    public Node(E e, Node<E> n){
+    private static class Node<E>{
+        private E data;
+        private Node<E> next;
+        public Node(E e, Node<E> n){
+            data = e;
+            next = n;
+        }
+        public E getElement(){
+            return data;
+        }
+        public Node<E> getNext(){
+            return next;
+        }
+        public void setNext(Node<E> n){
+            next = n;
+        }
     }
-    public E getElement(){
-    }
-    public Node<E> getNext(){
-    }
-    public void setNext(Node<E> n){
-    }
-  }
-  
-  public SinglyLinkedList(){
-  }
-  
-  public int size(){
-  }
-  
-  public boolean isEmpty(){
-  }
-  
-  public E first(){
-  }
-  
-  public E last(){
-  }
-  
-  public void addFirst(E element){
-  }
-  
-  public void addLast(E element){
-  }
-  
-  public E removeFirst(){
-  }
 
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    // Default constructor for SinglyLinkedList creates empty list with null references
+    public SinglyLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if (size == 0) return true;
+        else return false;
+    }
+
+    public E first(){
+        return head.getElement();
+    }
+
+    public E last(){
+        return tail.getElement();
+    }
+
+    public void addFirst(E element){
+        if (isEmpty() == true) {
+            Node<E> currentNode = new Node<E>(element, head);
+            head = currentNode;
+            tail = currentNode;
+            size++;
+        }
+        else {
+            Node<E> currentNode = new Node<E>(element, head);
+            head = currentNode;
+            size++;
+        }
+    }
+
+    public void addLast(E element){
+        if (isEmpty() == true) {
+            Node<E> currentNode = new Node<E>(element, null);
+            head = currentNode;
+            tail = currentNode;
+            size++;
+        }
+        else {
+            Node<E> currentNode = new Node<E>(element, null);
+            tail.setNext(currentNode);
+            tail = currentNode;
+        }
+    }
+
+    public E removeFirst(){
+        if (isEmpty() == true) return null;
+        else {
+            E returnedData = head.getElement();
+            head = head.getNext();
+            size--;
+            return returnedData;
+        }
+    }
 }
